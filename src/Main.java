405729c30ee3f98c3e346dbb999acb860089bebd
montimaj <monti.majumdar@gmail.com>
2017-05-01 11:59:53 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,126 @@
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ * @author Sayantan Majumdar (monti.majumdar@gmail.com)
+ */
+public class Main {
+    private ArrayList<Product> mProductList;
+    private FileInputStream mExcelFile;
+
+    private static final Main MAIN = new Main();
+    private static final float NIL_VALUE = 9999999.99f;
+
+    Main() {
+        mProductList = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        try {
+            MAIN.mExcelFile = new FileInputStream(args[0]);
+            MAIN.initAllProducts(args);
+            MAIN.generateOptimalProducts();
+            MAIN.mExcelFile.close();
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+
+    private void initAllProducts(String[] sheets) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook(mExcelFile);
+        for(int i = 1; i < sheets.length; ++i) {
+            Sheet sheet = workbook.getSheetAt(Integer.parseInt(sheets[i]));
+            for (Row nextRow : sheet) {
+                Iterator<Cell> cellIterator = nextRow.cellIterator();
+                Product product = getProduct(cellIterator);
+                if (product != null) {
+                    product.setVendor(sheet.getSheetName());
+                    Product product1 = getProduct(product);
+                    if (product1 != null && product1.getPrice() > product.getPrice()) {
+                        mProductList.remove(product1);
+                    }
+                    if (!mProductList.contains(product)) mProductList.add(product);
+                }
+            }
+            workbook.close();
+        }
+    }
+
+    private Product getProduct(Product product) {
+        for(Product product1: mProductList) {
+            if(product1.equals(product))    return product1;
+        }
+        return null;
+    }
+
+    private Product getProduct(Iterator<Cell> cellIterator) throws IOException {
+        Product product = new Product();
+        while (cellIterator.hasNext()) {
+            Cell cell = cellIterator.next();
+            int columnIndex = cell.getColumnIndex();
+            if (columnIndex == 0) {
+                try {
+                    int sl = (int) cell.getNumericCellValue();
+                    if(sl == 0) return null;
+                    product.setSerialNo(sl);
+                } catch (IllegalStateException e) { return null; }
+            } else if (columnIndex == 1) {
+                product.setName(cell.getStringCellValue());
+            } else if (columnIndex == 3) {
+                float price;
+                price = (float) cell.getNumericCellValue();
+                if(price == 0.0f)   price = NIL_VALUE;
+                product.setPrice(price);
+            }
+        }
+        return product;
+    }
+
+    private void generateOptimalProducts() throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Quotation");
+        String[] columns = {"SL", "Vendor", "Item & Specification", "Rate Exclusive of Vat"};
+        Row row = sheet.createRow(0);
+        for (int colNum = 0; colNum < columns.length; ++colNum) {
+            Cell cell = row.createCell(colNum);
+            cell.setCellValue(columns[colNum]);
+        }
+        mProductList.sort(Product.SERIAL_NO);
+        for(int rowNum = 0; rowNum < mProductList.size(); ++rowNum) {
+            row = sheet.createRow(rowNum + 1);
+            Product product = mProductList.get(rowNum);
+            for(int colNum = 0; colNum < Product.NUM_FIELDS; ++colNum) {
+                Cell cell = row.createCell(colNum);
+                if(colNum == 0) {
+                    cell.setCellValue(product.getSerialNo());
+                } else if(colNum == 1) {
+                    cell.setCellValue(product.getVendor());
+                } else if(colNum == 2) {
+                    cell.setCellValue(product.getName());
+                } else {
+                    float price = product.getPrice();
+                    if(price == NIL_VALUE)  cell.setCellValue("-");
+                    else {
+                        cell.setCellValue(product.getPrice());
+                        CellStyle style = workbook.createCellStyle();
+                        DataFormat format = workbook.createDataFormat();
+                        style.setDataFormat(format.getFormat("#,##0.00"));
+                        cell.setCellStyle(style);
+                    }
+                }
+            }
+        }
+        FileOutputStream outputStream = new FileOutputStream("Quotation.xlsx");
+        workbook.write(outputStream);
+        workbook.close();
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,55 @@
+import java.util.Comparator;
+
+/**
+ * @author Sayantan Majumdar (monti.majumdar@gmail.com)
+ */
+
+public class Product implements Comparable<Product> {
+    private String mName;
+    private String mVendor;
+    private float mPrice;
+    private int mSerialNo;
+
+    public static final Comparator<Product> SERIAL_NO = Comparator.comparingInt(o -> o.mSerialNo);
+    public static final int NUM_FIELDS = 4;
+
+    public Product() {}
+
+    public Product(int serialNo, String vendor, String name, float price) {
+        mSerialNo = serialNo;
+        mVendor = vendor.substring(vendor.indexOf("-") + 1);
+        if(mVendor.isEmpty())   mVendor = vendor;
+        mName = name;
+        mPrice = price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null || !(obj instanceof Product)) return false;
+        Product other = (Product) obj;
+        return mName.equalsIgnoreCase(other.mName);
+    }
+
+    @Override
+    public int compareTo(Product other) {
+        return SERIAL_NO.compare(this, other);
+    }
+
+    @Override
+    public String toString() {
+        return mSerialNo + ", " + mVendor + ", " +  mName + ", " + mPrice;
+    }
+
+    public void setVendor(String vendor) {
+        mVendor = vendor.substring(vendor.indexOf("-") + 1);
+        if(mVendor.isEmpty())   mVendor = vendor;
+    }
+    public void setName(String name) { mName = name; }
+    public void setPrice(float price) { mPrice = Float.parseFloat(String.format("%.2f", price)); }
+    public void setSerialNo(int serialNo) { mSerialNo = serialNo; }
+
+    public String getVendor() { return mVendor; }
+    public String getName() { return  mName; }
+    public float getPrice() { return  mPrice; }
+    public int getSerialNo() { return mSerialNo; }
+}
